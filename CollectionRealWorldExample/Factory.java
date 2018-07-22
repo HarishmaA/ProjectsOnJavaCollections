a53a35b15759cf3b91d46c35a14eafd8c91efd6f
Harishma Arivazhagan <29046089+HarishmaA@users.noreply.github.com>
2018-07-22 20:44:55 +0530

    Update Factory.java

--- CollectionRealWorldExample/Factory.java
+++ CollectionRealWorldExample/Factory.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 
 public class Factory {
 
-	HashMap<String, Bike> h = new HashMap<>();
+	HashMap<String, Bike> bikesProduced = new HashMap<>();
 
 	public Factory() {
-		h.put("royal enfield", new Bike("Royal Enfield", 95.6, 80.2, 6));
-		h.put("yamaha", new Bike("Yamaha", 70.6, 95.2, 3));
-		h.put("dio", new Bike("Dio", 80.6, 97.2, 5));
-		h.put("honda", new Bike("Honda", 90.0, 97.6, 4));
+		bikesProduced.put("royal enfield", new Bike("Royal Enfield", 95.6, 80.2, 6));
+		bikesProduced.put("yamaha", new Bike("Yamaha", 70.6, 95.2, 3));
+		bikesProduced.put("dio", new Bike("Dio", 80.6, 97.2, 5));
+		bikesProduced.put("honda", new Bike("Honda", 90.0, 97.6, 4));
 	}
 
 	ArrayList<Bike> arrayList = new ArrayList<>();
@@ -19,7 +19,7 @@ public class Factory {
 	ArrayList<Bike> order(String bikeName, int items) {
 		if (h.containsKey(bikeName.toLowerCase())) {
 			for (int i = 0; i < items; i++) {
-				arrayList.add(h.get(bikeName));
+				arrayList.add(bikesProduced.get(bikeName));
 			}
 		}
 		return arrayList;
