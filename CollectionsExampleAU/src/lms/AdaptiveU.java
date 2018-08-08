40415e79dbc41cbdcd39a2c293bee6bc6ec87941
Harishma Arivazhagan <29046089+HarishmaA@users.noreply.github.com>
2018-08-08 17:53:38 +0530

    Add files via upload

---
+++ CollectionsExampleAU/src/lms/AdaptiveU.java
@@ -0,0 +1,74 @@
+package lms;
+
+public class AdaptiveU {
+	private String identificationName;
+	private String nameOfTheChallenge;
+	private int time;
+	private double rating;
+	private String category;
+	private String videoLink;
+	private String type;
+	private boolean seen;
+
+	// Constructor
+	public AdaptiveU(String identificationName, String nameOfTheChallenge, int time, double rating, String category,
+			String videoLink, String type, boolean seen) {
+		this.identificationName = identificationName;
+		this.nameOfTheChallenge = nameOfTheChallenge;
+		this.time = time;
+		this.rating = rating;
+		this.category = category;
+		this.videoLink = videoLink;
+		this.type = type;
+		this.seen = seen;
+	}
+
+	String getIdentificationName() {
+		return this.identificationName;
+	}
+
+	int getTime() {
+		return this.time;
+	}
+
+	boolean getSeen() {
+		return this.seen;
+	}
+
+	void setSeen(boolean seen) {
+		this.seen = seen;
+	}
+
+	@Override
+	public String toString() {
+		return "\t" + identificationName + "\t" + nameOfTheChallenge + "\t" + time + "\t" + rating + "\t" + rating
+				+ "\t" + category + "\t" + videoLink + "\t" + type + "\t" + "seen";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+
+		if (obj == null)
+			return false;
+		if (!(obj instanceof AdaptiveU))
+			return false;
+		if (obj == this)
+			return true;
+		if (!this.identificationName.equals(((AdaptiveU) obj).identificationName)) {
+			return false;
+		}
+		if (!this.nameOfTheChallenge.equals(((AdaptiveU) obj).nameOfTheChallenge)) {
+			return false;
+		}
+		if (!(this.time == ((AdaptiveU) obj).time)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		System.out.println("hashcode value is " + Integer.valueOf(identificationName.charAt(0)));
+		return Integer.valueOf(identificationName.charAt(0));
+	}
+}
---
+++ CollectionsExampleAU/src/lms/User.java
@@ -0,0 +1,106 @@
+package lms;
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class User {
+
+	public static void main(String[] args) {
+		char ch;
+		Scanner scanner = new Scanner(System.in);
+		RequestHandler requestHandler = new RequestHandler();
+		while (true) {
+			System.out.println();
+			System.out.println("WHAT DO YOU WANT TO DO??");
+			System.out.println("PRESS 0 TO EXIT");
+			System.out.println("1.ADD A CHALLENGE");
+			System.out.println("2.DISPLAY ALL ADDED CHALLENGES ");
+			System.out.println("3.DELETE ALL CHALLENGES");
+			System.out.println("4.TO KNOW THE EARNED POINTS");
+			System.out.println("5.HAVE YOU COMPLETED ANY ADDED CHALLENGE? IF YES,");
+			System.out.println("Enter your choice");
+			int selectedOption = Integer.parseInt(scanner.nextLine());
+			if (selectedOption == 0) {
+				System.out.println("THANK YOU!!!BYEE");
+				System.exit(0);
+			}
+			switch (selectedOption) {
+
+			// 1. ADD A CHALLENGE
+			case 1:
+				System.out.println();
+				int time;
+				String identificationName, nameOfTheChallenge, category, videoLink, type;
+				double rating;
+				boolean seen;
+				try {
+					System.out.println(
+							"Enter the identification name,\nAlways enter the important word from video which you can remember");
+					identificationName = scanner.nextLine();
+					System.out.println("Enter name of the challenge");
+					nameOfTheChallenge = scanner.nextLine();
+					System.out.println("Enter the time");
+					time = Integer.parseInt(scanner.nextLine());
+					System.out.println("Enter the rating");
+					rating = scanner.nextDouble();
+					scanner.nextLine();
+					System.out.println("Enter the category");
+					category = scanner.nextLine();
+					System.out.println("Paste the video link");
+					videoLink = scanner.nextLine();
+					System.out.println("Enter the type");
+					type = scanner.nextLine();
+					System.out.println("If you have completed the challenge type \"true\" else type \"false\"");
+					seen = scanner.nextBoolean();
+					scanner.nextLine();
+
+				}
+
+				catch (InputMismatchException e) {
+					throw new InputMismatchException("OOPS!!! YOU TYPED WRONG INPUT!!!");
+				}
+
+				requestHandler.addingChallenge(new AdaptiveU(identificationName, nameOfTheChallenge, time, rating,
+						category, videoLink, type, seen));
+				break;
+
+			// 2. DISPLAY ALL ADDED CHALLENGES
+			case 2:
+				requestHandler.displayAllAddedChallenges();
+				break;
+
+			// 3. DELETE ALL CHALLENGES
+			case 3:
+				requestHandler.deleteAllAddedChallenges();
+				break;
+
+			// 4. TO KNOW THE EARNED POINTS
+			case 4:
+				int result = requestHandler.addingPoints();
+				System.out.println("The number of points earned  are " + result);
+				if (result > 300)
+					System.out.println(
+							"Amazing!!!Keep it up..Great!!You can get yummy snacks from AdaptiveU store..Enjoyyy");
+				else if (result >= 150 && result < 300)
+					System.out
+							.println("Hey you crossed 150 points..Now you can get yummy snacks from AdativeU Store!!");
+				else
+					System.out.println(
+							"You didn't reach the target!!Atleast try to score 150\n to get snacks from AdaptiveU store");
+				break;
+
+			// 5. HAVE YOU COMPLETED ANY ADDED CHALLENGE? IF YES,
+			case 5:
+				System.out.println("Enter the identification name");
+				String idName = scanner.nextLine();
+				requestHandler.setTrueIfSeen(idName);
+				break;
+
+			default:
+				System.out.println("Enter a valid option!!!!");
+			}
+		}
+
+	}
+
+}
